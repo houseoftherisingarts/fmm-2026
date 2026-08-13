6a025e7248dca42912c938ba34da19e9ae8cfc23
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-13 08:49:14 -0400

    autosave: 2026-08-13 08:49:13

--- src/pages/PartenairesPage.tsx
+++ src/pages/PartenairesPage.tsx
@@ -17,7 +17,7 @@ import { SponsorOffer } from './CommanditairesPage';
 const FEATURED = [
   {
     name: 'Municipalité de Montpellier',
-    image: '/wix/partenaires/80000153.png',
+    image: '/wix/partenaires/logo-montpellier-trim.png',
     logo: true,
     bioFR: 'En accueillant le festival sur leurs terres, la municipalité permet un événement à grand déploiement, riche, et impossible à recréer dans les grands centres urbains.',
     bioEN: 'By hosting the festival on their land, the municipality enables a sweeping, rich event impossible to recreate in major urban centres.',
