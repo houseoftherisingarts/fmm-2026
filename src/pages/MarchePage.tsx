9fc369d46108707389522042dc4e73417c51c604
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-27 23:59:39 -0400

    autosave: 2026-08-27 23:59:39

--- src/pages/MarchePage.tsx
+++ src/pages/MarchePage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useBadgeAuBout } from '../contexts/BadgesContext';
 import { useUI } from '../contexts/AppContext';
 import SEO from '../components/SEO';
