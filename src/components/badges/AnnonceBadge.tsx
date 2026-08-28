72da288ff61885bde4ca39715621fdda7aa9ac0c
alextstlaurent <alex@lesalondesinconnus.com>
2026-08-28 09:19:49 -0400

    autosave: 2026-08-28 09:19:49

--- src/components/badges/AnnonceBadge.tsx
+++ src/components/badges/AnnonceBadge.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { X } from 'lucide-react';
 import { useUI } from '../../contexts/AppContext';
