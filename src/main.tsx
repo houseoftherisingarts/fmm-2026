02bb3b5376d3e17dce00bebb965e7e3c25e26116
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-09 11:37:27 -0400

    autosave: 2026-09-09 11:37:27

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import HarnaisInventaire from './dev/HarnaisInventaire';
 import './index.css';
+
+const HARNAIS = true; // TEMPORAIRE : vérification visuelle du survol inventaire, à retirer.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {HARNAIS ? <HarnaisInventaire /> : <App />}
   </React.StrictMode>,
 );
