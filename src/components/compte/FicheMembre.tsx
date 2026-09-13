532394b34f8bf2cfd432d8b85352e12a9915f7a9
alextstlaurent <alex@lesalondesinconnus.com>
2026-09-12 20:27:06 -0400

    autosave: 2026-09-12 20:27:06

--- src/components/compte/FicheMembre.tsx
+++ src/components/compte/FicheMembre.tsx
@@ -61,8 +61,7 @@ import BadgeVerifie from './BadgeVerifie';
 import ReglagesProfil from './ReglagesProfil';
 import EspaceVip from './EspaceVip';
 import AlertesPanel from './AlertesPanel';
-import MaCarte from '../carte/MaCarte';
-import MonKiosque from './MonKiosque';
+import OngletCarte from './OngletCarte';
 
 // Le dé de la vie est un vrai d20 en trois dimensions : il tire three.js
 // derrière lui, donc il arrive à part, quand l'onglet s'ouvre.
